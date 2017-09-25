772ed9b013403fe5dd3c5a0fff888fb24b4aaf2d
Ariel Ortega <26ariel.ortega@gmail.com>
2017-09-24 21:28:35 -0400

    Esto sería la librería implementada sobre las operaciones que estaban como prototipos en otro archivo.

---
+++ Analizador Léxico/lista_simbolos.c
@@ -0,0 +1,42 @@
+//librería JSON creada
+#include "json.h"
+
+n aux = NULL;
+n nodop = NULL;
+
+// Inserta en la lista el token recibido
+
+void insertar(token ele){
+	n nuevo = (nodo*) malloc(sizeof(nodo));
+    
+	if (nuevo == NULL) exit(1);
+	nuevo->sgte = NULL;
+	nuevo->t = ele;
+    
+	if(nodop == NULL) nodop = nuevo;
+	else{
+		aux = nodop;
+		while(aux->sgte != NULL) aux = aux->sgte;
+		aux->sgte = nuevo;
+	}
+}
+
+token extraer(){
+	aux = nodop;
+	nodop = nodop->sgte;
+	return aux->t;
+}
+
+int esta_vacia(){
+    if (nodop == NULL) return 1;
+    else return 0;
+}
+
+void listar(){
+    if (esta_vacia()) printf("la lista se encuentra vacia");
+    n aux = nodop;
+    while(aux != NULL){
+        printf("componentelexico:%d,lexema:%s ,direccion: %p\n",aux->t.complexico,aux->t.lexema,aux->sgte);
+        aux = aux->sgte;
+    }
+}
